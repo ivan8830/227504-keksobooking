fa142f7fdb72454f5ae97aa12a722016ff7777a4
ivan8830 <ivan8830@yandex.ru>
2017-08-20 21:23:05 +0500

    заполнил массив

--- js/map.js
+++ js/map.js
@@ -2,13 +2,13 @@ var users = [User1, User2, User3, User4, User5, User6, User7, User8];
 
 User1 = {
   'autor': {
-    'avatar': img/avatars/user01.png;
-  }
+    'avatar': ../img/avatars/user01.png;
+  };
   'offer': {
     'title': 'Большая уютная квартира';
-    'address': '330, 850';
+    'address': '330, 388';
     'price': 20000руб;
-    'type': house;
+    'type': flat;
     'rooms': 5;
     'guests': 15;
     'checkin': 12:00;
@@ -16,9 +16,170 @@ User1 = {
     'features':[wifi, dishwasher, parking, washer, elevator, conditioner];
     'description':;
     'photos': [];
-  }
+  };
   'location': {
     'x': 330;
-    'y': 850;
+    'y': 388;
+  };
+};
+
+User2 = {
+  'autor': {
+    'avatar': ../img/avatars/user02.png;
+  };
+  'offer': {
+    'title': 'Маленькая неуютная квартира';
+    'address': '450, 450';
+    'price': 15000руб;
+    'type': flat;
+    'rooms': 1;
+    'guests': 9;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[washer, elevator, conditioner];
+    'description':;
+    'photos': [];
+  };
+  'location': {
+    'x': 450;
+    'y': 450;
+  };
+};
+
+User3 = {
+  'autor': {
+    'avatar': ../img/avatars/user03.png;
+  };
+  'offer': {
+    'title': 'Маленький ужасный дворец';
+    'address': '540, 270';
+    'price': 15000руб;
+    'type': house;
+    'rooms': 3;
+    'guests': 10;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[parking, washer, conditioner];
+    'description':;
+    'photos': [];
   }
-}
+  'location': {
+    'x': 540;
+    'y': 270;
+  };
+};
+
+User4 = {
+  'autor': {
+    'avatar': ../img/avatars/user04.png;
+  };
+  'offer': {
+    'title': 'Огромный прекрасный дворец';
+    'address': '500, 150';
+    'price': 150000руб;
+    'type': house;
+    'rooms': 8;
+    'guests': 22;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[parking, washer, conditioner];
+    'description':;
+    'photos': [];
+  }
+  'location': {
+    'x': 500;
+    'y': 150;
+  };
+};
+
+User5 = {
+  'autor': {
+    'avatar': ../img/avatars/user05.png;
+  };
+  'offer': {
+    'title': 'Красивый гостевой домик';
+    'address': '800, 200';
+    'price': 25000руб;
+    'type': house;
+    'rooms': 1;
+    'guests': 8;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[wifi, dishwasher, parking, washer, conditioner];
+    'description':;
+    'photos': [];
+  }
+  'location': {
+    'x': 800;
+    'y': 200;
+  };
+};
+
+User6 = {
+  'autor': {
+    'avatar': ../img/avatars/user06.png;
+  };
+  'offer': {
+    'title': 'Некрасивый гостевой домик';
+    'address': '650, 495';
+    'price': 30000руб;
+    'type': house;
+    'rooms': 1;
+    'guests': 8;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[wifi, parking];
+    'description':;
+    'photos': [];
+  }
+  'location': {
+    'x': 650;
+    'y': 495;
+  };
+};
+
+User7 = {
+  'autor': {
+    'avatar': ../img/avatars/user07.png;
+  };
+  'offer': {
+    'title': 'Уютное бунгало далеко от моря';
+    'address': '590, 188';
+    'price': 15000руб;
+    'type': bungalo;
+    'rooms': 1;
+    'guests': 4;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[wifi, parking, conditioner];
+    'description':;
+    'photos': [];
+  }
+  'location': {
+    'x': 590;
+    'y': 188;
+  };
+};
+
+User8 = {
+  'autor': {
+    'avatar': img/avatars/user08.png;
+  };
+  'offer': {
+    'title': 'Неуютное бунгало по колено в воде';
+    'address': '790, 588';
+    'price': 20000руб;
+    'type': bungalo;
+    'rooms': 1;
+    'guests': 4;
+    'checkin': '13:00';
+    'checkout': '12:00';
+    'features':[wifi, parking, conditioner];
+    'description':;
+    'photos': [];
+  }
+  'location': {
+    'x': 790;
+    'y': 588;
+  };
+};

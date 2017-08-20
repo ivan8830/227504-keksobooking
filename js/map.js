6fcdecc7719d4debdace8dd688669d7f78e58987
ivan8830 <ivan8830@yandex.ru>
2017-08-20 20:28:57 +0500

    Начал работу

---
+++ js/map.js
@@ -0,0 +1,24 @@
+var users = [User1, User2, User3, User4, User5, User6, User7, User8];
+
+User1 = {
+  'autor': {
+    'avatar': img/avatars/user01.png;
+  }
+  'offer': {
+    'title': 'Большая уютная квартира';
+    'address': '330, 850';
+    'price': 20000руб;
+    'type': house;
+    'rooms': 5;
+    'guests': 15;
+    'checkin': 12:00;
+    'checkout': 12:00;
+    'features':[wifi, dishwasher, parking, washer, elevator, conditioner];
+    'description':;
+    'photos': [];
+  }
+  'location': {
+    'x': 330;
+    'y': 850;
+  }
+}

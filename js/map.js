2409fc8f8abab2be6108307af46ac9faf8418720
ivan8830 <ivan8830@yandex.ru>
2017-08-24 03:45:36 +0500

    Почти конфетка

--- js/map.js
+++ js/map.js
@@ -184,61 +184,62 @@ User8 = {
     'y': 588,
   },
 };*/
-var ava = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+var avatarMassif = ['01', '02', '03', '04', '05', '06', '07', '08'];
 var comfort = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var entry = ['12:00', '12:00', '12:00', '13:00', '13:00', '14:00', '14:00'];
-var departure = ['12:00', '12:00', '12:00', '13:00', '13:00', '14:00', '14:00'];
+var featuresMassif = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var entry = ['12:00', '13:00', '14:00'];
+var departure = ['12:00', '13:00', '14:00'];
+var typeMassif = ['flat', 'house', 'bungalo'];
 
-function myRandom(from, to) {
+function getRandomNumber(from, to) {
   return Math.floor((Math.random() * (to - from + 1)) + from);
 }
 
+function getRandomValue(n) {
+  return Math.floor(Math.random() * n.length);
+}
+
+function getRandomMassif(c) {
+  var b = Math.floor(Math.random() * c.length);
+  c.length = b;
+  return c;
+}
+
+
 var users = [];
 for (var i = 0; i < 8; i++) {
-  var avatar = ava[i];
-  var title = comfort[i];
-  var checkin = entry[i];
-  var checkout = departure[i];
-  var features;
-  if (title === 'Большая уютная квартира' || title === 'Огромный прекрасный дворец' || title === 'Красивый гостевой домик') {
-    features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  } else if (title === 'Маленькая неуютная квартира' || title === 'Маленький ужасный дворец' || title === 'Некрасивый негостеприимный домик') {
-    features = ['parking', 'conditioner'];
-  } else if (title === 'Уютное бунгало далеко от моря') {
-    features = ['wifi', 'dishwasher', 'parking', 'washer', 'conditioner'];
-  } else {
-    features = ['parking', 'washer'];
-  }
-  var type;
-  if (title === 'Большая уютная квартира' || title === 'Маленькая неуютная квартира') {
-    type = 'flat';
-  } else if (title === 'Огромный прекрасный дворец' || title === 'Маленький ужасный дворец' || title === 'Красивый гостевой домик' || title === 'Некрасивый негостеприимный домик') {
-    type = 'house';
-  } else {
-    type = 'bungalo';
-  }
+  var avatar = getRandomValue(avatarMassif);
+  var title = getRandomValue(comfort);
+  var checkin = getRandomValue(entry);
+  var checkout = getRandomValue(departure);
+  var features = getRandomMassif(featuresMassif);
+  var type = getRandomValue(typeMassif);
+  var xMap = getRandomNumber(300, 900);
+  var yMap = getRandomNumber(100, 500);
   var User = {
     author: {
-      avatar: avatar
+      avatar: 'img/avatars/user' + avatarMassif[avatar] + '.png'
     },
     offer: {
-      title: title,
-      address: 'users[i].location[x], users[i].location[y]',
-      price: myRandom(1000, 1000000),
-      type: type,
-      rooms: myRandom(1, 5),
-      quests: myRandom(2, 15),
-      checkin: checkin,
-      checkout: checkout,
+      title: comfort[title],
+      address: xMap + ', ' + yMap,
+      price: getRandomNumber(1000, 1000000),
+      type: typeMassif[type],
+      rooms: getRandomNumber(1, 5),
+      quests: getRandomNumber(2, 15),
+      checkin: entry[checkin],
+      checkout: departure[checkout],
       features: features,
       description: '',
       photos: []
     },
     location: {
-      x: myRandom(300, 900),
-      y: myRandom(100, 500)
+      x: xMap,
+      y: yMap
     }
   };
+  avatarMassif.splice(avatar, 1);
+  comfort.splice(title, 1);
   users.push(User);
 }
 
@@ -247,9 +248,9 @@ var fragment = document.createDocumentFragment();
 for (var j = 0; j < 8; j++) {
   var newElement = document.createElement('div');
   newElement.className = 'pin';
-  newElement.style.left = 'users[j].location[x]px';
-  newElement.style.top = 'users[j].location[y]px';
-  newElement.innerHTML = '<img src="users[j].author[avatar]" class="rounded" width="40" height="40">';
+  newElement.style.left = users[j].location.x + 'px';
+  newElement.style.top = users[j].location.y + 'px';
+  newElement.innerHTML = '<img src=' + users[j].author.avatar + ' class=rounded width=' + 40 + ' height=' + 40 + '>';
   fragment.appendChild(newElement);
 }
 
